35258d5da1089d84ca46809052fd0a8d8d621864
mohammadkhan <mohammadkhan@digitalocean.com>
2025-08-26 22:57:17 +0530

    bump: upgrade to 0.3.0

--- src/parser.c
+++ src/parser.c
@@ -9384,7 +9384,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_proto(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 2,
+      .minor_version = 3,
       .patch_version = 0,
     },
   };
